e4cb8ed6012c3caf979df7fe4c7d8d1dfb1be531
moreirathomas <moreirathomas97@gmail.com>
2020-03-28 12:04:11 +0100

    Updated package.json and exported start() in start.js

--- start.js
+++ start.js
@@ -1,19 +1,23 @@
 import { game, selectPlayer, selectOpponent } from './src/gameMechanics';
 
-// Starting the game
-const player = selectPlayer();
-const opponent = selectOpponent();
-const battle = game(player, opponent);
-// console.log(battle);
+export const start = () => {
+  // Starting the game
+  const player = selectPlayer();
+  const opponent = selectOpponent();
+  const battle = game(player, opponent);
+  // console.log(battle);
 
-while (player.currentHp > 0 && opponent.currentHp > 0) {
-  let playerMove = battle.playerMove();
-  let opponentMove = battle.opponentMove();
-  let playerGoesFirst = battle.speedCheck();
-  battle.turnResolve(playerMove, opponentMove, playerGoesFirst);
-}
-if (opponent.currentHp <= 0) {
-  console.log('You won!');
-} else if (player.currentHp <= 0) {
-  console.log('You lose...');
-}
+  while (player.currentHp > 0 && opponent.currentHp > 0) {
+    let playerMove = battle.playerMove();
+    let opponentMove = battle.opponentMove();
+    let playerGoesFirst = battle.speedCheck();
+    battle.turnResolve(playerMove, opponentMove, playerGoesFirst);
+  }
+  if (opponent.currentHp <= 0) {
+    console.log('You won!');
+  } else if (player.currentHp <= 0) {
+    console.log('You lose...');
+  }
+};
+
+// start();
